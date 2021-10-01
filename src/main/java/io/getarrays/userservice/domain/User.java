8c4a9aa5f9ecf5698d935b5a3dfd80bfd6491166
Dumilde Matos <dumildejosematos@gmail.com>
2021-10-01 09:36:21 +0100

    Add User and Role domain classes

---
+++ src/main/java/io/getarrays/userservice/domain/User.java
@@ -0,0 +1,22 @@
+package io.getarrays.userservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static javax.persistence.FetchType.*;
+
+@Entity @Data @NoArgsConstructor @AllArgsConstructor
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String usename;
+    private String password;
+    @ManyToMany(fetch = EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+}
