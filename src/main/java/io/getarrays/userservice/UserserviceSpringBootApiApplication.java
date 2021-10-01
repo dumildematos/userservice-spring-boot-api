23abf845e8f685a0402b0c9a3aad7e020527aff3
Dumilde Matos <dumildejosematos@gmail.com>
2021-10-01 09:16:43 +0100

    first commit

---
+++ src/main/java/io/getarrays/userservice/UserserviceSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package io.getarrays.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserserviceSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserserviceSpringBootApiApplication.class, args);
+	}
+
+}
